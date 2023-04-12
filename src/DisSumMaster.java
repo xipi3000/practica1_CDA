6a2a8a7822d9c16cd030d43db143254dc4b4d553
Ant <antoniolopez18022002@gmail.com>
2023-04-12 22:21:32 +0200

    changed implementation, still wrong tho

--- src/DisSumMaster.java
+++ src/DisSumMaster.java
@@ -1,9 +1,5 @@
-import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.NotBoundException;
-import java.rmi.Remote;
+
 import java.rmi.RemoteException;
-import java.util.StringTokenizer;
 
 public class DisSumMaster {
     /*
@@ -11,20 +7,19 @@ public class DisSumMaster {
     (N) y la dirección donde se ejecuta el servidor de RMI (por defecto será localhost).
     Sintaxis: DisSumMaster <intervalo_final_sum> <#trabajos> [<ip_servidor>]
     */
-    public static void main(String[] args) throws RemoteException, MalformedURLException, NotBoundException {
+    public static void main(String[] args) throws RemoteException{
         //Set parameters given
         long last = Integer.parseInt(args[0]); //numero final a sumar
         long jobs = Integer.parseInt(args[1]); //Nº tareas
-        String ip_server = "localhost";
-        if (args.length > 2){
-            ip_server = args[3]; //ip servidor [localhost si no existe]
-        }
         //Get default queues
-        String registration = "rmi://" + ip_server + "/prac1";
-        Remote remoteService = Naming.lookup(registration);
-        MsgQ queue = (MsgQ) remoteService;
-        queue.MsgQ_CreateTopic("Work", EPublishMode.TOPIC); //usamos su método asociado para crear el Topic
-        queue.MsgQ_CreateQueue("Results"); //usamos otro método para crear una cola tipo P2P
+        String reg = "localhost";
+        if(args.length > 2){
+            reg = args[0];
+        }
+        MsgQClient client = new MsgQClient();
+        client.MsqQ_Init(reg);
+        client.MsgQ_CreateTopic("Work", EPublishMode.TOPIC); //usamos su método asociado para crear el Topic
+        client.MsgQ_CreateQueue("Results"); //usamos otro método para crear una cola tipo P2P
         //Distribute jobs
         long numeros_por_tarea = last / jobs; //proporción numeros a sumar por tarea
         String message;
@@ -41,22 +36,23 @@ public class DisSumMaster {
                 last_sum = last_sum + numeros_por_tarea;
                 message = first_sum + "-" + last_sum;
             }
-            queue.MsgQ_Publish("Work", message, 1); //type = 1 -> intervals
+            client.MsgQ_Publish("Work", message, 1); //type = 1 -> intervals
         }
         //Wait for results (since it's not blocking, we need the counter and to check for not null)
         int jobs_done = 0;
         long res = 0;
         while (jobs_done < jobs){
-            String msg = queue.MsgQ_ReceiveMessage("Results", 0); //Type s'haurà de mirar
+            String msg = client.MsgQ_ReceiveMessage("Results", 0); //Type s'haurà de mirar
             if (msg !=null){
                 jobs_done++;
                 long partialRes = processMessage(msg);
                 res+=partialRes;
             }
         }
+        System.out.println("S'ha acabat de sumar, resultat final: "+res);
         //Close queues after finishing job
-        queue.MsgQ_CloseTopic("Work");
-        queue.MsgQ_CloseQueue("Results");
+        client.MsgQ_CloseTopic("Work");
+        client.MsgQ_CloseQueue("Results");
     }
 
     private static long processMessage(String msg) {
--- src/DisSumWorker.java
+++ src/DisSumWorker.java
@@ -1,40 +1,24 @@
-import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.NotBoundException;
-import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.StringTokenizer;
 
 public class DisSumWorker implements TopicListenerInterface{
+    long tareas_calculadas;
     public DisSumWorker(){}
-    public static void main(String[] args) throws MalformedURLException, NotBoundException, RemoteException {
-        /*
-        El programa trabajador recibirá un único parámetro, la dirección del servidor de RMI (por defecto será localhost).
-        Sintaxis: DisSumWorker [<ip_servidor>]
-        */
-        MsgQ queue;
+    public static void main(String[] args) throws RemoteException {
+        //get queue from client implementation
         String reg = "localhost";
         if(args.length > 0){
             reg = args[0];
         }
-        String registration = "rmi://" + reg + "/prac1";
-        Remote remoteService = Naming.lookup(registration);
-        queue = (MsgQ) remoteService;
-        /*
-        El programa trabajador al principio de la ejecución se tendrá que subscribir al tema de “Log” y mostrar los
-        mensajes recibidos por pantalla.
-        */
+        MsgQClient client = new MsgQClient();
+        client.MsqQ_Init(reg);
+        //sub to Log
         DisSumWorker listen = new DisSumWorker();
         TopicListenerInterface listener = (TopicListenerInterface) UnicastRemoteObject.exportObject(listen, 0);
-        queue.MsgQ_Subscribe("Log", listener);
-        /*
-        Sera el programa encargado de realizar las tareas de sumatorio de un intervalo de números.
-        El trabajador se suscribirá a la cola de mensajes “Work” y a partir de ese momento, cada vez que el listener
-        reciba un mensaje, calculará el sumatorio del intervalo de números asociado con la tarea y enviará el resultado
-        parcial al máster mediante el envío de un mensaje a la cola “Results”.
-        */
-        String msg = queue.MsgQ_ReceiveMessage("Work", 1);
+        client.MsgQ_Subscribe("Log", listener);
+        //retrieve interval, calculate and send to "Results" queue
+        String msg = client.MsgQ_ReceiveMessage("Work", 1);
         StringTokenizer stok = new StringTokenizer(msg, "-");
         long first = (long) stok.nextElement();
         long last = (long) stok.nextElement();
@@ -42,11 +26,8 @@ public class DisSumWorker implements TopicListenerInterface{
         for (long i=first; i<last; i++){
             res+=i;
         }
-        queue.MsgQ_SendMessage("Results", String.valueOf(res), 2);
-        /*
-        Una vez el trabajador sea notificado que la cola “Work” ha sido cerrada, el programa finalizará mostrando por
-        pantalla el número de tareas que ha procesado.
-        */
+        client.MsgQ_SendMessage("Results", String.valueOf(res), 2);
+        listen.tareas_calculadas++;
     }
 
     @Override
@@ -56,6 +37,11 @@ public class DisSumWorker implements TopicListenerInterface{
 
     @Override
     public void onTopicClosed(String topic) throws RemoteException {
-
+        /*
+        Una vez el trabajador sea notificado que la cola “Work” ha sido cerrada, el programa finalizará mostrando por
+        pantalla el número de tareas que ha procesado.
+        */
+        System.out.println("Se ha terminado la ejecución del programa.");
+        System.out.println("Tareas calculadas: "+tareas_calculadas);
     }
 }
